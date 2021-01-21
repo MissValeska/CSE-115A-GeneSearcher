9db80358709d541fd8790e80c8a2b9acf6c34d73
Charles Knight <Charles-Knight@users.noreply.github.com>
2021-01-21 00:09:26 -0800

    Implemented simple test program to show opensnp_parser and user_data_parser working together

--- GeneSearcherTestProgram.py
+++ GeneSearcherTestProgram.py
@@ -9,8 +9,13 @@ if __name__ == "__main__":
     # Read users genetic data from file
     user_genetic_data = user_data_parser.parse_user_data(file)
     for rsid in user_genetic_data:
-        print(rsid, ":", user_genetic_data[rsid])
+        print(rsid, ":", user_genetic_data[rsid], end="")
         traits = parser.fetch_RSID_info(rsid)
-        print(traits)
+        if len(traits) != 0:
+            genotype = (user_genetic_data[rsid][2][0],user_genetic_data[rsid][2][1])
+            print(" - " , opensnp_Parser.match_genotype(traits, genotype))
+            #print(traits)
+        else:
+            print(" - No data exists for this RSID")
 
     #for rsid in user_genetic_data
