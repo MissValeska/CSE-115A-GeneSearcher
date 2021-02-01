06e7308d065e2c40c2f8a5142a1c67c4ea744347
Charles Knight <Charles-Knight@users.noreply.github.com>
2021-01-31 17:29:49 -0800

    Created data_collector module. A multithreaded module that uses the opensnp parser to collect data about a list of RSIDs from opensnp

---
+++ data_collector.py
@@ -0,0 +1,56 @@
+from opensnp_parser import opensnp_Parser
+import json, requests, sys, threading, queue
+
+class data_collector():
+
+    def __init__(self, t_lim):
+        self.interesting_RSIDs = list()
+        self.RSID_data = {}
+        self.q = queue.Queue()
+        self.thread_limit = t_lim
+    
+    def update_worker(self):
+        parser = opensnp_Parser()
+        while True:
+            while not self.q.empty():
+                rsid = self.q.get()
+                data = parser.fetch_RSID_info(rsid)
+                self.RSID_data[rsid] = data
+                # print(data)
+                self.q.task_done()
+
+    
+    # Takes a list of RSIDs and updates the RSID_data and interesting_RSIDs
+    def update(self, RSIDs):
+        
+        # init threads
+        for i in range(self.thread_limit):
+            threading.Thread(target=self.update_worker, daemon=True).start()
+
+        # Enqueu RSIDS
+        for RSID in RSIDs:
+            self.q.put(RSID)
+        print("RSID lookups enqued")
+        self.q.join()
+        print("Processing finished")
+    
+    # reloads the structure from a JSON file
+    def load(self, file):
+        pass
+    
+    # writes the dataset to a JSON file
+    def write(self, file):
+        pass
+
+    def print(self):
+        print("RSID data stored in collector:")
+        for rsid in self.RSID_data:
+            print(rsid, " - ", self.RSID_data[rsid])
+
+
+if __name__ == "__main__":
+    rsid_list = ["rs548049170", "rs4988235"]
+    dc = data_collector(5)
+    dc.update(rsid_list)
+    print("Collection complete")
+    dc.print()
