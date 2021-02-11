d0a163645e5d31badaaa5058fb932f281ae84be5
Charles Knight <Charles-Knight@users.noreply.github.com>
2021-02-11 13:19:05 -0800

    heavily modified GenSearcherProcessData to load local data file, load user data, and then process users data. Made small modification to opensnp_parser to allow for single allele genotypes and have functioning prototype of processing user data via Firebase

--- GeneSearcherProcessData.py
+++ GeneSearcherProcessData.py
@@ -2,30 +2,57 @@ import user_data_parser
 from opensnp_parser import opensnp_Parser
 import sys, time, requests, json
 
+def load_user_data(data_file):
+    user_data = user_data_parser.parse_user_data(data_file)
+    return user_data
+
+def load_data_set_from_file(data_file):
+    data_set = dict()
+    with open(data_file) as f:
+        data_set = json.load(f) 
+    return data_set
+
+def process_user_data(user_data, data_set):
+    # Including opensnp parser to use it's functions for matching user genotype
+    # to dataset. But we should probably put that functionality elsewhere at
+    # this point.
+    parser = opensnp_Parser(1)
+
+    report = dict()
+    for rsid in user_data:
+        if rsid in data_set:
+            user_genotype = user_data[rsid][2]
+            # print(rsid, " -", user_genotype)
+            if user_genotype not in {"--", "DD", "II"}:
+                report[rsid] = parser.match_genotype(data_set[rsid], user_genotype)
+    return report
+
+
 if __name__ == "__main__":
-    file = sys.argv[1]
+    user_data_file = sys.argv[1]
+    data_file = sys.argv[2]
+
     parser = opensnp_Parser(5)
 
     # Read users genetic data from file
-    user_genetic_data = user_data_parser.parse_user_data(file)
-    ### Below is short dummy list of testing. To test comment above line
-    #   out and uncomment below
-    # user_genetic_data = {"rs548049170" : ["","","CT"], "rs4988235" : ["","","CT"]}
-    rsids = list()
-    
-    # Generate list of RSID's to request
-    for rsid in user_genetic_data:
-        rsids.append(rsid)
+    user_genetic_data = load_user_data(user_data_file)
+    data_set = load_data_set_from_file(data_file)
+    report = process_user_data(user_genetic_data, data_set)
 
-    # Have the parser make the requests.
-    data = parser.fetch_bulk_RSID_info(rsids)
-
-    for rsid in data:
-        if len(data[rsid]) > 0: 
-            print(rsid, " - ", data[rsid])
-            genotype = user_genetic_data[rsid][2]
-            print("  User has ", genotype, " - " , parser.match_genotype(data[rsid], genotype))
-
-        else:
-            print(rsid, " - no data for this value")
-
+    for item in report:
+        if report[item] not in {"common in clinva",
+                                "common in clinvar",
+                                "common in complete genomic",
+                                "common in complete genomics",
+                                "No summary provided",
+                                "norma",
+                                "normal",
+                                "Normal",
+                                "commo",
+                                "averag",
+                                "average",
+                                "common/normal",
+                                "common on affy axiom dat",
+                                "common on affy axiom data",
+                                None}:
+            print(item, " - ", report[item])
