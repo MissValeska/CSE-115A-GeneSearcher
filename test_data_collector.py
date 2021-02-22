7b62fcaa2755f2e1358cc6495edd6e8e1f84dadb
ConstantineSv <ksaryche@ucsc.edu>
2021-02-22 02:55:55 -0800

    Added tests for data collector

---
+++ test_data_collector.py
@@ -0,0 +1,28 @@
+import unittest
+from data_collector import data_collector
+
+class test_data_collector(unittest.TestCase):
+    def test_update_bulk(self):
+        # test if the function update updates rsids properly
+        collector = data_collector(1)
+        rsids = ["rs548049170", "rs9283150", "rs116587930", "rs3131972", "rs12184325"]
+        # what the dictionary should look like
+        dictionary = {"rs548049170"  :  ['1', '69869', 'TT'],
+                    "rs9283150"  :  ['1', '565508', 'AA'],
+                    "rs116587930"  :  ['1', '727841', 'GG'],
+                    "rs3131972"  :  ['1', '752721', 'AG'],
+                    "rs12184325"  :  ['1', '754105', 'CT']}
+        # what the interesting rsids list should be
+        list = rsids
+        collector.update_bulk(rsids)
+        self.assertEqual(collector.RSID_data, dictionary)
+        self.assertEqual(collector.interesting_RSIDs, list)
+        print(collector.RSID_data)
+        print (collector.interesting_RSIDs)
+        
+    def test_get_snp_list(self):
+    
+    
+if __name__ == "__main__":
+
+    unittest.main()
