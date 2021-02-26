cef679009cfbeb81390b41e6df6b0adb238148dd
Charles Knight <Charles-Knight@users.noreply.github.com>
2021-02-25 21:09:39 -0800

    Added controller main and model

---
+++ controller.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from model import Genesearcher_Model
+from gui import App
+import os
+
+class GeneSearcher_Controller:
+    def __init__(self):
+        self.model = Genesearcher_Model(self)
+        self.load_data_set()
+        self.root = tk.Tk()
+        self.view = App(self.root, self)
+        self.root.mainloop()
+
+    def load_data_set(self, filepath=None):
+        if filepath == None:
+            self.model.load_data_set_from_server()
+        else:
+            self.model.load_data_set_from_file(filepath)
+    
+    def input_data_file(self, filepath):
+        self.model.load_user_data(filepath)
+
+    def generate_report(self, text_frame):
+        '''
+        Primary backend linkage. Utilize functions from GeneSearcherProcessData.py
+        to load in user data from file and data from server to compare to. Generates
+        the report and sends it to display function. Bound to search_button in gui.py
+        PARAMS : text frame to display report in.
+        '''
+        print("Generating Report for: " + self.model.get_input_file())
+        report = self.model.generate_report()
+        self.view.display_report(text_frame, report)
---
+++ main.py
@@ -0,0 +1,4 @@
+from controller import GeneSearcher_Controller
+
+if __name__ == "__main__":
+    c = GeneSearcher_Controller()
