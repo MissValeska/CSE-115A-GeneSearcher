aa962438c845121bea5262db2ddc25ca5f12ad31
ConstantineSv <ksaryche@ucsc.edu>
2021-03-02 18:42:32 -0800

    updated test_data_collecor

--- test_data_collector.py
+++ test_data_collector.py
@@ -1,5 +1,6 @@
 import unittest
 from data_collector import data_collector
+from data_collector import get_snp_list
 
 class test_data_collector(unittest.TestCase):
     def test_update_bulk(self):
@@ -7,25 +8,31 @@ class test_data_collector(unittest.TestCase):
         collector = data_collector(1)
         rsids = ["rs548049170", "rs9283150", "rs116587930", "rs3131972", "rs12184325"]
         # what the dictionary should look like
-        dictionary = {"rs548049170"  :  ['1', '69869', 'TT'],
-                    "rs9283150"  :  ['1', '565508', 'AA'],
-                    "rs116587930"  :  ['1', '727841', 'GG'],
-                    "rs3131972"  :  ['1', '752721', 'AG'],
-                    "rs12184325"  :  ['1', '754105', 'CT']}
+        dictionary = {'rs116587930': {'traits': {}, 'weight': 0},
+                    'rs12184325': {'traits': {}, 'weight': 0},
+                    'rs3131972': {'traits': {}, 'weight': 0},
+                    'rs548049170': {'traits': {}, 'weight': 0},
+                    'rs9283150': {'traits': {}, 'weight': 0}}
+                    
         # what the interesting rsids list should be
         list = rsids
         collector.update_bulk(rsids)
         self.assertEqual(collector.RSID_data, dictionary)
         self.assertEqual(collector.interesting_RSIDs, list)
-        print(collector.RSID_data)
-        print (collector.interesting_RSIDs)
+      
     
     # check if the function gets snp list correctly
     def test_get_snp_list(self):
-        list1 = get_snp_list(snp_list.txt)
+        list1 = get_snp_list("snp_list.txt")
         list2 = ["rs548049170", "rs9283150", "rs116587930", "rs3131972", "rs12184325"]
-        self.assertEqual(list1, list2)
+        self.assertEqual(sorted(list1), sorted(list2))
     
+    # check if the function loads the data properly
+    def test_load(self):
+        collector = data_collector()
+        with open ('condensed_data_log_v3.json') as file:
+           data = json.load(file)
+           self.aserEqual(data, collector.load('condensed_data_log_v3.json')
     
 if __name__ == "__main__":
 
